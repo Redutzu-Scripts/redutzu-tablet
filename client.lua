94570dd5cdb509ebbf4f2c899a5552883f85c0c3
Redutzu <ffabi1499@gmail.com>
2022-04-10 16:03:54 +0300

    animation

--- client.lua
+++ client.lua
@@ -1,12 +1,80 @@
+local open = false
+local object = nil
+
+local anim = Config.AnimDict
+local prop = Config.Prop
+local bone = Config.Bone
+
+-- Utils
+local function LoadDict(dict)
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Wait(100)
+    end
+end
+
+local function LoadProp(model)
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(100)
+    end
+end
+
+-- Toggle tablet & animation
+local function ToggleTablet(state)
+    local ped = PlayerPedId()
+    open = state
+    
+    if state then
+        LoadDict(anim)
+
+        if not object then
+            LoadProp(prop)
+
+            local coords = GetEntityCoords(ped)
+            local hand = GetPedBoneIndex(ped, bone)
+
+            object = CreateObject(GetHashKey(prop), coords, 1, 1, 1)
+            AttachEntityToEntity(object, ped, hand, 0.0, 0.0, 0.03, 0.0, 0.0, 0.0, 1, 1, 0, 1, 0, 1)
+        end
+
+        if not IsEntityPlayingAnim(ped, anim, 'base', 3) then
+            TaskPlayAnim(ped, anim, 'base', 8.0, 1.0, -1, 49, 1.0, 0, 0, 0)
+        end
+
+        SetNuiFocus(state, state)
+        SendNUIMessage({ 
+            action = 'open' 
+        })
+    else
+        DeleteEntity(object)
+        DetachEntity(object, 1, 1)
+        ClearPedTasks(ped)
+
+        SetNuiFocus(state, state)
+        SendNUIMessage({ 
+            action = 'close' 
+        })
+    end
+end
+
+-- Event for server-side
+RegisterNetEvent('redutzu-tablet:client:open', function()
+    ToggleTablet(true)
+end)
+
+-- Close 
 RegisterNUICallback('close', function(data, cb)
-    SetNuiFocus(false, false)
-    SendNUIMessage({ action = 'close' })
+    ToggleTablet(false)
     cb('ok')
 end)
 
+-- Command / Keybind
+
 RegisterCommand(Config.Command, function()
-    SetNuiFocus(true, true)
-    SendNUIMessage({ action = 'open' })
+    ToggleTablet(true)
 end, false)
 
-RegisterKeyMapping(Config.Command, Config.CommandDescription, 'KEYBOARD', Config.OpenKey)
+if Config.Keybind then
+    RegisterKeyMapping(Config.Command, Config.CommandDescription, 'KEYBOARD', Config.OpenKey)
+end
--- config.lua
+++ config.lua
@@ -2,4 +2,10 @@ Config = {}
 
 Config.Command = 'help'
 Config.CommandDescription = 'Opens the information tablet'
+
+Config.Keybind = true
 Config.OpenKey = 'F2'
+
+Config.AnimDict = 'amb@world_human_seat_wall_tablet@female@base'
+Config.Prop = 'prop_cs_tablet'
+Config.Bone = 28422
